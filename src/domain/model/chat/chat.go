9c45d37b3c57533ffbf525fc97fce53177b978ee
hiroky1983 <hirockysan1983@gmail.com>
2024-01-19 18:50:02 +0900

    チャットのテーブル作成

--- src/domain/model/chat/chat.go
+++ src/domain/model/chat/chat.go
@@ -9,13 +9,9 @@ import (
 type Chat struct {
 	bun.BaseModel `bun:"table:chat,alias:c"`
 
-	ID        string    `bun:"id,pk,type:uuid,default:gen_random_uuid()"`
-	UserName 	string    `bun:"type:varchar(255)"`
-	Email     string    `bun:"type:varchar(255)"`
-	Password  string    `bun:"type:varchar(255)"`
-	ImagePath string    `bun:"type:varchar(255)"`
-	IsVerified bool     `bun:"default:false"`
-	GoogleID  string    `bun:"type:varchar(255)"`
+	ID        string    `json:"id" bun:"id,pk,type:int,autoincrement"`
+	Message 	string    `json:"message" bun:"type:varchar(255),notnull"`
+	UserID    string    `json:"user_id" bun:"type:uuid,notnull"`
 	CreatedAt time.Time `json:"created_at" bun:"default:current_timestamp"`
 	UpdatedAt time.Time `json:"updated_at" bun:"default:current_timestamp"`
 	IsDeleted bool      `json:"is_deleted" bun:"default:false"`
---
+++ src/migrate/migrations/20240119011658_chat.go
@@ -0,0 +1,30 @@
+package migrations
+
+import (
+	"context"
+	"homework/domain/model/chat"
+
+	"github.com/uptrace/bun"
+)
+
+func init() {
+	Migrations.MustRegister(func(ctx context.Context, db *bun.DB) error {
+		_, err := db.NewCreateTable().
+			Model((*chat.Chat)(nil)).
+			ForeignKey(`(user_id) REFERENCES "user" (id) ON DELETE CASCADE`).
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	}, func(ctx context.Context, db *bun.DB) error {
+		_, err := db.NewDropTable().
+			Model((*chat.Chat)(nil)).
+			IfExists().
+			Exec(ctx)
+		if err != nil {
+			return err
+		}
+		return nil
+	})
+}
