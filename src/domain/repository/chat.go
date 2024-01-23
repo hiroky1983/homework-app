bcb08087bfab689ef0754262898c0167f8a0454e
hiroky1983 <hirockysan1983@gmail.com>
2024-01-24 01:18:31 +0900

    moq作成のコード追加

--- src/domain/repository/chat.go
+++ src/domain/repository/chat.go
@@ -5,6 +5,8 @@ import (
 	"homework/domain/model/chat"
 )
 
+//go:generate moq -pkg fakerepository -out ../../moq/fakerepository/chat_test_moq.go . IChatRepository
+
 type IChatRepository interface {
 	Create(db DBConn, user *chat.Chat) error
 	ListChatByUserID(db DBConn, chatList *chat.ChatList) error
--- src/domain/repository/user.go
+++ src/domain/repository/user.go
@@ -6,6 +6,8 @@ import (
 	"homework/domain/model/user"
 )
 
+//go:generate moq -pkg fakerepository -out ../../moq/fakerepository/user_test_moq.go . IUserRepository
+
 type IUserRepository interface {
 	GetUserByEmail(db DBConn, user *user.User, email string) error
 	CreateUser(db DBConn, user *user.User) error
