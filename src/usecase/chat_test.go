d9e8a7e42d6aa9e6c2a258ae9c955a53c3d7b490
hiroky1983 <hirockysan1983@gmail.com>
2024-01-25 23:05:11 +0900

    test記述

--- src/usecase/chat_test.go
+++ src/usecase/chat_test.go
@@ -1,10 +1,14 @@
 package usecase
 
 import (
+	chatModel "homework/domain/model/chat"
 	"homework/domain/repository"
+	"homework/moq/fakerepository"
 	"reflect"
 	"testing"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/uptrace/bun"
 )
 
@@ -18,12 +22,82 @@ func TestNewChatUsecase(t *testing.T) {
 		args args
 		want IChatUsecase
 	}{
-
+		{
+			name: "success",
+			args: args{
+				ur: nil,
+				db: nil,
+			},
+			want: &chatUsecase{
+				ur: nil,
+				db: nil,
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewChatUsecase(tt.args.ur, tt.args.db); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewChatUsecase() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_chatUsecase_Create(t *testing.T) {
+	now := time.Now()
+	type fields struct {
+		ur repository.IChatRepository
+		db *bun.DB
+	}
+	type args struct {
+		c chatModel.Chat
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    chatModel.ChatResponse
+		wantErr bool
+	}{
+		{
+			name: "success",
+			fields: fields{
+				ur: &fakerepository.IChatRepositoryMock{
+					CreateFunc: func(db repository.DBConn, user *chatModel.Chat) error {
+						return nil
+					},
+				},
+			},
+			args: args{
+				c: chatModel.Chat{
+					ID:        1,
+					UserID:    uuid.New().String(),
+					Message:   "test",
+					CreatedAt: now,
+				},
+			},
+			want: chatModel.ChatResponse{
+				ID:        1,
+				Sender:    chatModel.SenderMe,
+				Message:   "test",
+				CreatedAt: now,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			cu := &chatUsecase{
+				ur: tt.fields.ur,
+				db: tt.fields.db,
+			}
+			got, err := cu.Create(tt.args.c)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("chatUsecase.Create() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("chatUsecase.Create() = %v, want %v", got, tt.want)
 			}
 		})
 	}
