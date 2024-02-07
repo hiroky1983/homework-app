e98c2bbae66dfff4dbe9df978b49f41fba7ae7fb
hiroky1983 <hirockysan1983@gmail.com>
2024-02-08 00:05:47 +0900

    lint

--- src/domain/model/chat/chat.go
+++ src/domain/model/chat/chat.go
@@ -18,7 +18,7 @@ type Chat struct {
 	ID        uint64    `json:"id" bun:"id,pk,autoincrement"`
 	Message   string    `json:"message" bun:"type:varchar(255),notnull"`
 	UserID    string    `json:"user_id" bun:"type:uuid,notnull"`
-	RoomID		uint64    `json:"room_id" bun:"type:uuid,notnull"`
+	RoomID    uint64    `json:"room_id" bun:"type:uuid,notnull"`
 	CreatedAt time.Time `json:"created_at" bun:"default:current_timestamp"`
 	UpdatedAt time.Time `json:"updated_at" bun:"default:current_timestamp"`
 	IsDeleted bool      `json:"is_deleted" bun:"default:false"`
@@ -30,7 +30,7 @@ type DeleteChatRequest struct {
 
 type ChatResponse struct {
 	ID        uint64    `json:"id" bun:"primary_key"`
-	Message   string    `json:"message"` 
+	Message   string    `json:"message"`
 	Sender    string    `json:"sender"`
 	CreatedAt time.Time `json:"created_at"`
 }
