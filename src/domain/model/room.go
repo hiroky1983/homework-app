cb8d1d0028eccc37d476ae00806e64edc5f70d5c
hiroky1983 <hirockysan1983@gmail.com>
2024-02-07 19:52:46 +0900

    roomのモデル定義

--- src/domain/model/room.go
+++ src/domain/model/room.go
@@ -1,2 +1,17 @@
-package model
+package room
 
+import (
+	"time"
+
+	"github.com/uptrace/bun"
+)
+
+type Room struct {
+	bun.BaseModel `bun:"table:room,alias:r"`
+
+	RoomID    uint64    `json:"id" bun:"id,pk,autoincrement"`
+	UserID    string    `json:"user_id" bun:"type:uuid,notnull"`
+	CreatedAt time.Time `json:"created_at" bun:"default:current_timestamp"`
+	UpdatedAt time.Time `json:"updated_at" bun:"default:current_timestamp"`
+	IsDeleted bool      `json:"is_deleted" bun:"default:false"`
+}
