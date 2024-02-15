a2945be32eeff9690898b1f4135031660cf10820
hiroky1983 <hirockysan1983@gmail.com>
2024-02-15 19:45:43 +0900

    websocketのパッケージをgorillaに変更

--- src/controller/chat.go
+++ src/controller/chat.go
@@ -1,8 +1,6 @@
 package controller
 
 import (
-	"encoding/json"
-	"fmt"
 	"homework/config"
 	"homework/domain/model/chat"
 	apperror "homework/error"
@@ -11,8 +9,9 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/websocket"
 	"github.com/labstack/echo/v4"
-	"golang.org/x/net/websocket"
+
 	"golang.org/x/oauth2"
 )
 
@@ -28,9 +27,16 @@ type chatController struct {
 	oauthConf *oauth2.Config
 }
 
-// var (
-// 	upgrader = websocket.Upgrader{}
-// )
+var (
+	upgrader = websocket.Upgrader{	
+		CheckOrigin: func(r *http.Request) bool {
+		// ここでオリジンの検証を行う場合があります。
+		// セキュリティ上の理由からオリジンを検証することが推奨されますが、
+		// 開発中や内部ツールの場合は true を返してすべて許可することがあります。
+		return true
+		},
+	}	
+)
 
 func NewChatController(uu usecase.IChatUsecase, cnf config.Config, oauthConf *oauth2.Config) IChatController {
 	return &chatController{uu, cnf, oauthConf}
@@ -38,95 +44,46 @@ func NewChatController(uu usecase.IChatUsecase, cnf config.Config, oauthConf *oa
 
 func (cc *chatController) HandleWebSocket(c echo.Context) error {
 	log.Println("Serving at web socket...")
+
 	userID, err := token.GetUserIDWithTokenCheck(c)
 	if err != nil {
 		return c.JSON(http.StatusUnauthorized, apperror.ErrorWrapperWithCode(err, http.StatusUnauthorized))
 	}
-	// ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	// if err != nil {
-	// 	fmt.Printf("Failed to set websocket upgrade: %+v\n", err)
-	// 	return err
-	// }
-	// defer ws.Close()
 
-	// for {
-	// 	// Write
-	// 	message := []byte("Hello, Client!")
-	// 	err := ws.WriteMessage(websocket.TextMessage, message)
-	// 	if err != nil {
-	// 		c.Logger().Error(err)
-	// 	}
+	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return err
+	}
+	defer ws.Close()
 
-	// 	req := chat.Chat{
-	// 		Message: "Hello, Server!",
-	// 		UserID:  userID.(string),
-	// 	}
-
-	// 	res, err := cc.cu.Create(req)
-	// 	if err != nil {
-	// 		fmt.Println(err)
-	// 		c.Logger().Error(err)
-	// 	}
-
-	// 	r, err := json.Marshal(res)
-	// 	fmt.Println(r)
-	// 		if err != nil {
-	// 			fmt.Println(err)
-	// 			c.Logger().Error(err)
-	// 		}
-	// 	// Read
-	// 	a, msg, err := ws.ReadMessage()
-	// 	fmt.Println(a)
-	// 	if err != nil {
-	// 		c.Logger().Error(err)
-	// 	}
-	// 	fmt.Printf("%s\n", msg)
-	// }
-
-	websocket.Handler(func(ws *websocket.Conn) {
-		defer ws.Close()
-		for {
-			// Client からのメッセージを読み込む
-			msg := ""
-			if err := websocket.Message.Receive(ws, &msg); err != nil {
-				fmt.Println(err)
-				c.Logger().Error(err)
-				break // エラー発生時にループを終了
-			}
-
-			// string to json
-			var chatreq chat.ChatRequest
-			if err := json.Unmarshal([]byte(msg), &chatreq); err != nil {
-				fmt.Println(err)
-				c.Logger().Error(err)
-			}
-			req := chat.Chat{
-				Message: chatreq.Message,
-				RoomID:  chatreq.RoomID,
-				UserID:  userID,
-			}
-
-			res, err := cc.cu.Create(req)
-			if err != nil {
-				fmt.Println(err)
-				c.Logger().Error(err)
-			}
-
-			r, err := json.Marshal(res)
-			if err != nil {
-				fmt.Println(err)
-				c.Logger().Error(err)
-			}
-
-			// 	// Client からのメッセージを元に返すメッセージを作成し送信する
-			err = websocket.Message.Send(ws, string(r))
-			if err != nil {
-				fmt.Println(err)
-				c.Logger().Error(err)
-			}
+	for {
+		// Client からのメッセージを読み込む
+		var chatreq chat.ChatRequest
+		if err := ws.ReadJSON(&chatreq); err != nil {
+			log.Println("Read error:", err)
+			break // エラー発生時にループを終了
 		}
 
-	}).ServeHTTP(c.Response(), c.Request())
+		req := chat.Chat{
+			Message: chatreq.Message,
+			RoomID:  chatreq.RoomID,
+			UserID:  userID,
+		}
+
+		res, err := cc.cu.Create(req)
+		if err != nil {
+			log.Println("Create error:", err)
+			continue // エラーが発生した場合は次のメッセージを待つ
+		}
+
+		// クライアントにレスポンスを送信する
+		if err := ws.WriteJSON(res); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
+
 	return nil
 }
 
