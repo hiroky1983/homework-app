a2539c09195b500869f24ccce24704b3db769525
hiroky1983 <hirockysan1983@gmail.com>
2024-02-19 23:03:03 +0900

    不要コメント削除

--- src/controller/chat.go
+++ src/controller/chat.go
@@ -1,7 +1,6 @@
 package controller
 
 import (
-	"fmt"
 	"homework/config"
 	"homework/domain/model/chat"
 	apperror "homework/error"
@@ -47,48 +46,12 @@ func (cc *chatController) CreateChat(c echo.Context) error {
 		UserID:  userID,
 	}
 	
-	fmt.Printf("req: %+v\n", req)
 	res, err := cc.cu.Create(chat)
 	if err != nil {
 		return c.JSON(500, err)
 	}
 
 	return c.JSON(200, res)
-	// ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
-	// if err != nil {
-	// 	log.Println("Upgrade error:", err)
-	// 	return err
-	// }
-	// defer ws.Close()
-
-
-
-	// for {
-	// 	// Client からのメッセージを読み込む
-	// 	var chatreq chat.ChatRequest
-	// 	if err := ws.ReadJSON(&chatreq); err != nil {
-	// 		log.Println("Read error:", err)
-	// 		break // エラー発生時にループを終了
-	// 	}
-
-	// 	req := chat.Chat{
-	// 		Message: chatreq.Message,
-	// 		RoomID:  chatreq.RoomID,
-	// 		UserID:  userID,
-	// 	}
-
-	// 	res, err := cc.cu.Create(req)
-	// 	if err != nil {
-	// 		log.Println("Create error:", err)
-	// 		continue // エラーが発生した場合は次のメッセージを待つ
-	// 	}
-
-	// 	// クライアントにレスポンスを送信する
-	// 	if err := ws.WriteJSON(res); err != nil {
-	// 		log.Println("Write error:", err)
-	// 		break
-	// 	}
-	// }
 }
 
 func (cc *chatController) ListChat(c echo.Context) error {
